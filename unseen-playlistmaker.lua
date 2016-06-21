aa16c31d5ca3df8ad4e135c3f348f579aa4a3574
donmaiq <kayhty@windowslive.com>
2016-06-22 01:32:48 +0300

    Updated more userfriendly settings

--- unseen-playlistmaker.lua
+++ unseen-playlistmaker.lua
@@ -1,90 +1,72 @@
--- Created by Jonni-e 9.2.2016
--- This lua file needs to be located in your mpv scripts folder, or called with --script=<path>
--- Windows: C:\Users\anon\AppData\Roaming\mpv\scripts
--- Linux: ~/.config/mpv/scripts/
---
--- This script is created for the purpose of saving someone from navigating in their 
--- download folder for unwatched episodes of shows.
---
--- Keybind 1: Playlist-mode-toggle(W), when activating playlist-mode all unwatched shows from 
--- media directory will be appended to the playlist, and new files be searched for on load and 80%playthrough.
--- When toggling off playlist-mode, automatic searches for files will no longer be done.
--- Skipped files in playlist-mode will append to the end of the playlist. 
--- Allowing you to skip over files if you want to see your unwatched shows in a certain order.
---
--- Keybind 2: mark-seen(w), will mark an unwatched episode as watched. 
--- Note that this will also happen automatically when video reaches 80%.
---
--- Without activating playlist-mode (W) this script will keep track of watched files.
--- If mpv is ran without a terminal then search() will cause momentary popup windows. 
--- Remember you can toggle it off after loading the playlist with another (W).
--- If you use --idle the script will continue to wait for files in idle mode if playlist-mode is active.
--- So for example to load all your unseen videos run "mpv --idle" and press keybind for Playlist-mode-toggle(W). 
--- Make sure your torrent client stores incompleted files somewhere else, or they won't be loaded again once complete
--- I have tried minimizing errors with incomplete files but better safe than sorry
--- Files that are being copied over will not be loaded incomplete.
------------------------------------------------------------------------------------------
+local settings = {
+    linux_over_windows = false,                                 --linux=true, windows=false
 
---EDIT this path below to where you want your text file of watched shows placed
---note the escape strings on windows paths and traling / or \ because path will be chained with filenames.
---Make sure you have read and write permissions in the scriptloc folder
---you might need to disable UAC with regedit if you want to save to system folders on windows.
---To test write permission just open mpv --idle, if the list.txt file is created then permissions should be fine.
---I suggest using an absolute path, otherwise list.txt location will not be absolute.
-local scriptloc="D:\\users\\anon\\Downloads(hdd)\\shortcuts\\scripts\\" 
+    --unseen playlistmaker settings
+    unseen_load_on_start = false,                                       --toggle to load unseen playlistmaker on startup, use only if loading script manually
+    unseen_filetypes = {'*mkv','*mp4'},                                 --unseen-playlistmaker filetypes, {'*'} for all filetypes
+    unseen_searchpath = "D:\\users\\anon\\Downloads(hdd)\\animu-temp\\",--path to media files where unseen-playlistmaker should look for files 
+    unseen_savedpath="X:\\code\\mpv\\list.txt"                          --file and path to where to save seen shows 
 
---Below is path to media files, note that this script is designed with only one media path in mind. 
-local fileloc="D:\\users\\anon\\Downloads(hdd)\\"
+}
 
-local filetypes = {'*mkv','*mp4'} -- add whatever you want in the same format
-local search =' '
-for w in pairs(filetypes) do
-    search = search..fileloc..filetypes[w]..' '
-    --search = search..'"'..fileloc..filetypes[w]..'" ' --alternative that quotes searches if path has spaces, windows only
-end
-
---change the scan below to suit your needs, note that all unwatched files this search finds, will try to be opened in mpv. 
---on default it searches all mkv and mp4 files in fileloc folder, and lists them one per line. 
---scanning files from subdirectories will break the for loop in search().
---whatever you scan, make sure the result is one file per line, filename only.
-
---local scan = 'find'..search..'-type f -printf "%f\\n"' --linux version, untested, fiddle around until it werks
-local scan = 'dir /b'..search --windows version
-
-
------------------------------------------------------------------------------------------
-local txtfile=scriptloc.."list.txt"
 local mp=require 'mp'
-local filename=nil
-local mark=false
+local os=require 'os'
 local seenarray={}
 local loadingarray={}
 local active = false
 local idle = nil
+local first = true
+local search =' '
+for w in pairs(settings.unseen_filetypes) do
+    if settings.linux_over_windows then
+        search = search..settings.unseen_searchpath..settings.unseen_filetypes[w]..' '
+    else
+        search = search..'"'..settings.unseen_searchpath..settings.unseen_filetypes[w]..'" '
+    end
+end
+if linux_over_windows then
+    scan = 'find'..search..'-type f -printf "%f\\n"'
+else
+    scan = 'dir /b'..search 
+end
 
 --creating a list.txt file if one doesn't exist
-local test, err= io.open(txtfile, "r")
+local test, err= io.open(settings.unseen_savedpath, "r")
 if not test then
     mp.msg.info("creating list.txt file")
-    local create = io.open(txtfile, "w")
+    local create = io.open(settings.unseen_savedpath, "w")
     if not create then mp.msg.info("Failed to create list.txt file, check permissions to path") else create:close() end
 else 
     test:close() 
 end
 
---file is loaded
 function on_load(event)
     filename = mp.get_property('filename')
     path = mp.get_property('path')
+    pos = mp.get_property('playlist-pos')
+    plen = tonumber(mp.get_property('playlist-count'))
+    fullpath = string.sub(mp.get_property("path"), 1, string.len(mp.get_property("path"))-string.len(mp.get_property("filename")))
     mark=false
     --check if file has duration. If it has one, start listening for progress. Streams are skipped this way.
     local dur = mp.get_property('duration')
     if dur then timecheck() else mark=true end
+
+    if settings.loadfiles_filetypes == true then
+        search_playlist = fullpath..'*'
+    else
+        search_playlist = ' '
+        for w in pairs(settings.loadfiles_filetypes) do
+            if settings.linux_over_windows then
+                search_playlist = search_playlist..fullpath..settings.loadfiles_filetypes[w]..' '
+            else
+                search_playlist = search_playlist..'"'..fullpath..settings.loadfiles_filetypes[w]..'" '
+            end
+        end
+    end
 end
 
 
 function on_close(event)
-    filename=nil
     --if playlist-mode is active, unwatched files are appended to end of playlist
     if mark == false and active and path then
         local oldfile = mp.get_property('playlist/'..(mp.get_property('playlist-pos')-1)..'/filename')
@@ -96,6 +78,7 @@ function on_close(event)
         end
         mp.commandv("loadfile", path, "append")
     end
+    filename=nil
     idle=mp.get_property('idle')
     if idle == 'yes' and active then 
        idle_timer('closed')
@@ -149,7 +132,7 @@ function watched(args)
         if args~='timer' then mp.msg.info("File already marked as watched: " .. filename) end
         return 
     end
-    local file, err = io.open(txtfile, "a+")
+    local file, err = io.open(settings.unseen_savedpath, "a+")
     if file==nil then
         mp.msg.info("Error opening list.txt in watched()")
     else
@@ -165,7 +148,7 @@ function watched(args)
         else
             if args~='timer' then mp.msg.info("File already marked as watched: " .. filename) end
         end
-    file:close()
+        file:close()
     end
 end
 
@@ -188,7 +171,7 @@ end
 --appends unseen episodes into playlist
 --if a new file is added to the folder, it will be appended on next search
 function search(args)
-    local seenlist= io.open(txtfile, "r")
+    local seenlist= io.open(settings.unseen_savedpath, "r")
     if seenlist == nil then mp.msg.info("Cannot write to list.txt file, check permissions or change path.") return end
     local seen=seenlist:read("*l")
     while seen ~= nil do
@@ -203,12 +186,12 @@ function search(args)
     for dirx in popen:lines() do
         if not seenarray[dirx] then
             --checking that file is not being copied
-            local errcheck = io.open(fileloc..dirx, "r") 
+            local errcheck = io.open(settings.unseen_searchpath..dirx, "r") 
             if errcheck then  
                 errcheck:close()
                 seenarray[dirx]='true'
                 count = count +1
-                mp.commandv("loadfile", fileloc..dirx, "append-play")
+                mp.commandv("loadfile", settings.unseen_searchpath..dirx, "append-play")
                 mp.msg.info("Appended to playlist: " .. dirx)
             end
         end
@@ -217,10 +200,19 @@ function search(args)
         mp.osd_message("Added total of: "..count.." files to playlist")
     end
     popen:close()
+    plen = tonumber(mp.get_property('playlist-count'))
 end
+
+if settings.unseen_load_on_start then
+    activate()
+end
+
+mp.register_event('file-loaded', on_load)
+mp.register_event('end-file', on_close)
 
 --change the lines below if you want to change keybindings
 mp.add_key_binding('w', 'mark-seen', watched)
 mp.add_key_binding('W', 'playlist-mode-toggle', activate)
-mp.register_event('file-loaded', on_load)
-mp.register_event('end-file', on_close)
+
+mp.add_key_binding('P', 'loadfiles', playlist)
+mp.add_key_binding('p', 'saveplaylist', save_playlist)
