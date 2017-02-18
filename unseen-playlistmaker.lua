9cf6d79625db2374024f8af8dc0fa04b53546d55
donmaiq <kayhty@windowslive.com>
2017-02-18 17:56:06 +0200

    Update unseen-playlistmaker.lua

--- unseen-playlistmaker.lua
+++ unseen-playlistmaker.lua
@@ -11,7 +11,7 @@ local settings = {
   --absolute path to media directory where unseen-playlistmaker should look for files. Do not use aliases like $HOME.
   --notice trailing slashes, escape backslashes on windows like c:\\dir\\
   unseen_directory = "/home/anon/Videos/",
-  --full path and name of file that contains seen files
+  --full path and name of file that contains seen files, don't keep as tmp if you want to save them
   seenlist_file = "/tmp/unseenlist",
 }
 
@@ -64,7 +64,7 @@ if not test then
   msg.info(err.." => creating seen list file")
   local create, err = io.open(settings.seenlist_file, "w")
   if not create then
-    msg.error("Failed to create seen list, check permissions or create manually. Error: "..err or "") 
+    msg.error("Failed to create seen list, check permissions or create manually. Error: "..(err or "")) 
   else 
     msg.info("File created without problems! The script is ready now.")
     create:close() 
@@ -79,10 +79,13 @@ function on_load()
   pos = mp.get_property_number('playlist-pos', 0)
   plen = mp.get_property_number('playlist-count', 0)
   directory = utils.split_path(path)
-  mark=false
+  mark = false
 
   --only track files that are in our searchdirectory
-  if directory == settings.unseen_directory then unseentimer:resume() else mark=true end
+  if directory == settings.unseen_directory then
+    local ismarked = seenarray[filename] == 'true'
+    if not ismarked then unseentimer:resume() end
+  end
 end
 
 function on_close()
@@ -97,9 +100,15 @@ function on_close()
     mp.commandv("loadfile", path, "append")
   end
 
+  --make sure watched cannot be called when no file is playing
+  filename = nil
+  --make sure unseentimer is killed so on_load can resume it if needed
+  unseentimer:kill()
+
+  --activate idle timer if player enters idle
   if mp.get_property('idle-active', 'no') == 'yes' and active then 
     msg.info("Entering idle mode and listening for new files.")
-  idletimer:resume()
+    idletimer:resume()
   end
 end
 
@@ -114,7 +123,7 @@ end
 
 --checks position of video and marks as watched
 function timecheck()
-  if mark or not filename then return end
+  if mark then unseentimer:kill() end
   local percentpos = mp.get_property_number('percent-pos', 0)
   --position in % when to mark file
   if percentpos >= 80 then
@@ -133,27 +142,60 @@ function watched(args)
   if not mark then
     mark = true
   else
-    if args~='timer' then msg.warn("File already marked as watched: "..filename) end
-    return
+    if args == 'timer' then return end
+    if args == 'true' or not args then
+      msg.warn("File already marked as watched: "..filename)
+      return
+    end
   end
   local file, err = io.open(settings.seenlist_file, "a+")
   if not file then
-    msg.error("Error opening seen list in watched() : "..err or "")
+    msg.error("Error opening seen list in watched() : "..(err or ""))
   else
+    local content = {}
     local line = file:read("*l")
     local match = false
     while line ~= nil do
-      if line == filename then 
+      if line == filename then
         match = true
-        break
+        --stop iterating if we will not rewrite the file
+        if args == 'timer' or args == 'true' then 
+          break
+        end
+      else
+        --create list of seen files without the current file incase of removal
+        content[#content+1] = line
       end
       line = file:read("*l")
     end
     if not match then
       msg.info("Marking as watched: " .. filename)
-      file:write(filename, "\n")
+      file:write(string.format( "%s\n", filename))
+      --set file as watched
+      seenarray[filename] = 'true'
+      unseentimer:kill()
     else
-      if args ~= 'timer' then msg.warn("File already marked as watched: "..filename) end
+      --if file is marked but we want to remove it
+      if args == 'toggle' or args == 'false' then
+        file:close()
+        local file, err = io.open(settings.seenlist_file, "w+" )
+        if not file then
+          msg.error("Error trying to rewrite seen list: "..(err or ""))
+        else
+          for i = 1, #content do
+            file:write( string.format( "%s\n", content[i] ) )
+          end
+          file:close()
+          msg.info("Removing from watched: " .. filename)
+          mark = false
+          --set file into queue status
+          seenarray[filename] = 'queue'
+          return
+        end
+      elseif args ~= 'timer' then
+        msg.warn("File already marked as watched: "..filename)
+        unseentimer:kill()
+      end
     end
     file:close()
   end
@@ -177,10 +219,11 @@ end
 --if a new file is added to the folder, it will be appended on next search
 function search(args)
   local seenlist, err = io.open(settings.seenlist_file, "r")
-  if not seenlist then msg.error("Cannot read seen list: "..err or "") return end
+  if not seenlist then msg.error("Cannot read seen list: "..(err or "")) ; return end
   local seen = seenlist:read("*l")
   while seen ~= nil do
     if not seenarray[seen] then
+      --mark files from seen list as true into seenarray
       seenarray[seen]='true'
     end
     seen = seenlist:read("*l")
@@ -194,7 +237,8 @@ function search(args)
       local errcheck, err = io.open(utils.join_path(settings.unseen_directory, line), "r") 
       if errcheck then
         errcheck:close()
-        seenarray[line]='true'
+        --mark loaded files as queue
+        seenarray[line]='queue'
         count = count + 1
         mp.commandv("loadfile", utils.join_path(settings.unseen_directory, line), "append-play")
         msg.info("Loaded: "..line)
@@ -219,15 +263,17 @@ if settings.load_on_start then
 end
 
 --react to script messages
-function unseenmsg(msg, value)
+function unseenmsg(msg, value, reason)
+  --print for debugging messages
+  --print(msg, value, "reason: "..(reason or ""))
+
   --allows other scripts to mark file to avoid conflicts
-  if msg == "mark" then mark = value=="true" ; return end
+  if msg == "mark" then mark = value=="true" ; if value=="true" then unseentimer:kill() else unseentimer:resume() end; return end
   if msg == "search" then search(value) ; return end
   if msg == "activate" and value=="true" then activate(true) ; return end
   if msg == "activate" and value=="false" then activate(false) ; return end
   if msg == "activate" and value==nil then activate() ; return end
-  if msg == "mark-seen" then watched() ; return end
-
+  if msg == "mark-seen" then watched(value) ; return end
 end
 mp.register_script_message("unseenplaylist", unseenmsg)
 mp.register_event('file-loaded', on_load)
